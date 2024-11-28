0c453dbc88f61e5da104cd3835918570cfce4f1b
Jhonatas Kévin <jhonataskevin552@gmail.com>
2024-11-28 20:09:15 -0300

    Create estruturas_condicionais.py

---
+++ estruturas_condicionais.py
@@ -0,0 +1,12 @@
+# Estruturas de condicionais
+
+nota = 85
+
+if nota >= 90:
+    print("Exelente")
+elif nota >= 80:
+    print("muito bom")   # Perceba que if no começo, depois vem o elif(quantas vezes for necessaria), e sempre no final o else.
+elif nota >= 70:
+    print("Bom")
+else:
+    print("precisa melhorar")
