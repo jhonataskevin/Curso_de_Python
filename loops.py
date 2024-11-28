08db4a5af665b53d76c9c29c5f5ec849fe765968
Jhonatas Kévin <jhonataskevin552@gmail.com>
2024-11-28 20:08:56 -0300

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,12 @@
+print("Numeros de 1 a 5 multiplicando por 2 com o laço for")
+for numero in range(1, 6):
+    print(numero * 2)
+
+print("\nNumeros de 1 a 5 multiplicando por 2 com laço while:")
+contador = 1
+while contador <= 5:
+    print(contador  * 2)
+    contador +=1
+
+print("_______________________________________")
+
