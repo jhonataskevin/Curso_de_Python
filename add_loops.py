70d7004909d4ca026436d67f128a2a4b9494c1e2
Jhonatas Kévin <jhonataskevin552@gmail.com>
2024-11-28 20:08:45 -0300

    Create add_loops.py

---
+++ add_loops.py
@@ -0,0 +1,20 @@
+contadr = 0
+
+while True:
+    
+    print(contadr)
+    contadr +=1
+
+    if contadr == 5:
+        break # usada para sair do loop
+    
+    
+
+for i in range(10):
+
+    if i % 2 == 0:
+        continue # Ultilizada para pular o restante do loop.
+    print(i)
+
+for p in range(5):
+    pass # operação nula, que não faz nada.
