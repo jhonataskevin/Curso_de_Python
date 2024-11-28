45d24d503851225d553eaeef5e9122f098c6910b
Jhonatas Kévin <jhonataskevin552@gmail.com>
2024-11-28 20:09:23 -0300

    Create aritmeticos.py

---
+++ aritmeticos.py
@@ -0,0 +1,13 @@
+# Aritméticos
+
+a = 10
+b = 3
+
+
+soma = a + b   # 13
+subtracao = a - b    # 7
+multiplicacao = a * b    # 30
+divisao = a / b   # 3.333333333
+divisao_inteira = a // b   # 3 - ele divide o primeiro valor pelo segundo e devolve um resultado do tipo inteiro.
+modulo = a % b   # 1 resto
+exponenciacao = a ** b   # 1000 - ele eleva o primeiro valor a potencia do segundo.
